9a50ad103e9d038d470b8125b2e39075ca693d07
Pavel Revak <pavel.revak@gmail.com>
2015-08-02 14:03:18 +0200

    setuptools to distutils

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-import setuptools
+import distutils.core
 
-setuptools.setup(
+distutils.core.setup(
     name='gpiosam',
     description='Atmel AT91SAM GPIO Python driver',
     author='Pavel Revak',
     author_email='pavel.revak@gmail.com',
     url='http://github.com/pavelrevak/gpiosam',
-    version='0.1',
+    version='1.0.0',
+    license='MIT',
     py_modules=['gpiosam'],
 )
