8a2e9ee08d9d157ca184fed2d6c31554ffaad18a
Pavel Revak <pavel.revak@gmail.com>
2015-08-02 13:07:06 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='gpiosam',
+    description='Atmel AT91SAM GPIO Python driver',
+    author='Pavel Revak',
+    author_email='pavel.revak@gmail.com',
+    url='http://github.com/pavelrevak/gpiosam',
+    version='0.1',
+    py_modules=['gpiosam'],
+)
